31dec3b1f98ad08746c83fa92cdcc0815f6f924e
Zack Harley <zackharley@gmail.com>
2018-05-15 00:09:27 -0400

    Open itinerary in browser

--- server/api.js
+++ server/api.js
@@ -9,13 +9,12 @@ router.get('/itinerary/:date', async (req, res) => {
     try {
         const date = moment(req.params.date).format('YYYY-MM-DD');
         const itineraryFile = await generateItinerary({ date });
-        // const stream = fs.createReadStream(itineraryFile);
-        // const stat = fs.statSync(itineraryFile);
-        // res.setHeader('Content-Length', stat.size);
-        // res.setHeader('Content-Type', 'application/pdf');
-        // res.setHeader('Content-Disposition', `inline; filename=itinerary-${date}.pdf`);
-        // return stream.pipe(res);
-        res.sendFile(itineraryFile);
+        const stream = fs.createReadStream(itineraryFile);
+        const stat = fs.statSync(itineraryFile);
+        res.setHeader('Content-Length', stat.size);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `attachment; filename=itinerary-${date}.pdf`);
+        return stream.pipe(res);
     } catch (error) {
         return res.status(400).send({ message: 'Error!', error: error.toString() });
     }
